015031a31f696d9e2a68c1b03dea20d24eb85ff7
Rafael Sebastião Miranda <rafael.sebastiaomiranda@mail.utoronto.ca>
2015-08-30 14:27:16 -0300

    Update t4.hs

--- t4.hs
+++ t4.hs
@@ -1,3 +1,5 @@
+import Data.Char (toLower)
+
 -- Exercício 1
 
 eleva2 :: [Int] -> [Int]
@@ -12,3 +14,22 @@ contido c (s:sx) = (c==s) || contido c sx
 
 -- Exercício 3
 
+semVogais :: String -> String
+semVogais [] = []
+semVogais (c:cx) = if (toLower c=='a') || (toLower c=='e') || (toLower c=='i') || (toLower c=='o') || (toLower c=='u')
+   then semVogais cx
+   else c : semVogais cx
+   
+-- Exercício 4
+
+translate :: [(Double,Double)] -> [(Double,Double)]
+translate [] = []
+translate ((p1,p2):tx) = (p1+2,p2+2) : translate tx
+
+-- Exercício 5
+
+geraTabela' :: Int -> [(Int,Int)]
+geraTabela' 1 = [(1,1)]
+geraTabela' n = if n>1
+   then geraTabela' (n-1) ++ [(n,n^2)]
+   else undefined
