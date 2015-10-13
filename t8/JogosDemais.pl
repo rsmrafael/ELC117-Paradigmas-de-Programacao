8374eebd9a3fc26d896dcfc9acbd501c22c0bd14
Rafael Sebastião Miranda <rafael.sebastiaomiranda@mail.utoronto.ca>
2015-10-12 23:51:18 -0300

    add comments

--- t8/JogosDemais.pl
+++ t8/JogosDemais.pl
@@ -9,6 +9,7 @@
 % Base
 
 /* jogos retorna lista com possível combinação de jogos e custos */
+% Um dos jogos custa R$ 1, 00 por dia jogado, outro custa R$ 1, 20 por dia jogado, e o outro custa R$ 1, 50 por dia jogado
 custo(J,C) :- member(J,[x,y,z]) , member(C,[1,1.2,1.5]).
 jogos(JS) :- custo(x,JX),custo(y,JY),custo(z,JZ),
 			not(JX=JY ; JX=JZ ; JY=JZ),
@@ -25,10 +26,10 @@ week(W) :-  jogos(JS) , JS = [(x,JX),(y,JY),(z,JZ)],
 			member(J5,JS) , member(J6,JS),
 			W = [J2,J3,J4,J5,J6],
 			member((x,JX),W),member((y,JY),W),member((z,JZ),W),
-			J5 = (_,1.5),
-			JX > JZ,
-			J4=(_,P4),J6=(_,P6),P4 > P6,
-			count((z,JZ),W,CZ),count((x,JX),W,CX),CZ>CX.
+			J5 = (_,1.5), % Às quintas-feiras, João joga o jogo que custa R$ 1, 50.
+			JX > JZ, % O jogo X custa mais do que o jogo Z.
+			J4=(_,P4),J6=(_,P6),P4 > P6, % O jogo que João joga às quartas-feiras é mais caro do que o jogo que ele joga às sextas-feiras.
+			count((z,JZ),W,CZ),count((x,JX),W,CX),CZ>CX. % João joga o jogo Z mais vezes por semana do que ele joga o jogo X.
 			
 % Questão 7
 			
@@ -39,7 +40,7 @@ check7(C) :- week([(_,C2),(_,C3),(_,C4),(_,C5),(_,C6)]),
 	% ?- check7(5).
 	% false.
 	% ?- check7(6.2).
-	% true .
+	% true . (Resposta certa (c))
 	% ?- check7(7.5).
 	% false.
 	% ?- check7(8).
@@ -50,7 +51,7 @@ check7(C) :- week([(_,C2),(_,C3),(_,C4),(_,C5),(_,C6)]),
 check8(J2,J3,J4,J5,J6) :- week([(J2,_),(J3,_),(J4,_),(J5,_),(J6,_)]).
 
 	% ?- check8(y,z,x,y,z).
-	% true .
+	% true . (Resposta certa (a))
 	% ?- check8(y,z,z,y,x).
 	% false.
 	% ?- check8(z,z,x,x,y).
@@ -77,4 +78,4 @@ check9(D) :- D=2 , week([(_,1.5),_,_,_,_]);
 	% ?- check9(5).
 	% true .
 	% ?- check9(6).
-	% false.
+	% false. (Resposta certa (e))
